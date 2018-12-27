ba3478b47d72c9b1dcfde3b61fef76b60f08c969
Alexander Pervachuk <apervachuk@wiley.com>
2018-12-27 16:41:45 +0300

    New commit

--- src/test/java/DemoLocalTest.java
+++ src/test/java/DemoLocalTest.java
@@ -1,7 +1,5 @@
 import com.codeborne.selenide.ElementsCollection;
-import io.qameta.allure.Step;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 import java.util.concurrent.TimeUnit;
 
@@ -9,6 +7,8 @@ import static com.codeborne.selenide.Selectors.byName;
 import static com.codeborne.selenide.Selectors.byXpath;
 import static com.codeborne.selenide.Selenide.*;
 import static org.assertj.core.api.Assertions.assertThat;
+
+//import io.qameta.allure.Step;
 
 /**
  * Created by Alexander Pervachuk <apervachuk@wiley.com> on 27.12.2018 8:33
@@ -20,7 +20,6 @@ public class DemoLocalTest extends BaseLocalTest {
     public void test() {
 
         open("https://www.wikipedia.org/");
-
         step1();
         try {
             TimeUnit.SECONDS.sleep(5);
@@ -37,7 +36,7 @@ public class DemoLocalTest extends BaseLocalTest {
 
     }
 
-    @Step
+    //    @Step
     private void step1(){
         System.out.println("step1");
     }
