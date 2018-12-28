994d06499148de1ef0fefddf7a4d5e2fc3758ae2
Alexander Pervachuk <piligrim18@gmail.com>
2018-12-28 22:46:47 +0300

    another commit from home

--- src/test/java/runner/RunnerTest.java
+++ src/test/java/runner/RunnerTest.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 
 @CucumberOptions(
-        plugin = {"html:target/cucumber-report/smoketest", "json:target/cucumber.json"},
+        plugin = {"html:target/cucumber-report/smoketest", "json:target/cucumber.json", "pretty"},
         features = "src/test/resources/features",
         glue = "",
         tags = "@smoketest"
